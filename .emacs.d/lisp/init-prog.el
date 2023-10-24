c8b68ec34a4d16bbb675c85e25a493becfd16631
akijoey <akijoey@akijoey.com>
2023-10-24 16:31:34 +0000

    feat: lsp mode config

--- .emacs.d/lisp/init-prog.el
+++ .emacs.d/lisp/init-prog.el
@@ -13,15 +13,32 @@
   (setq show-paren-when-point-inside-paren t)
   (setq show-paren-delay 0))
 
+;; lsp mode
+(defun lsp-bash-check-zsh-shell (&rest _)
+  "Check whether ‘sh-shell’ is sh, bash, or zsh"
+  (and (memq major-mode '(sh-mode bash-ts-mode))
+    (memq sh-shell '(sh bash zsh))))
+
 (use-package lsp-mode
   :hook
   (prog-mode . (lambda ()
     (unless (derived-mode-p 'emacs-lisp-mode 'lisp-mode 'makefile-mode)
       (lsp-deferred))))
-  (lsp-mode . lsp-enable-which-key-integration))
+  (lsp-mode . lsp-enable-which-key-integration)
+  :bind (:map lsp-mode-map
+    ([remap xref-find-definitions] . lsp-find-definition)
+    ([remap xref-find-references] . lsp-find-references))
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (setq lsp-modeline-code-action-fallback-icon "!")
+  (advice-add 'lsp-bash-check-sh-shell
+    :override 'lsp-bash-check-zsh-shell))
 
 (use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode))
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (setq lsp-ui-sideline-enable nil))
 
 (use-package lsp-treemacs
   :after (lsp-mode treemacs)
