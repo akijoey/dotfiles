7f8994e5c7e0310d979497b28079d4e6070631e3
akijoey <akijoey@akijoey.com>
2024-01-16 00:24:47 +0000

    feat: global prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,24 @@
 const { execSync } = require('child_process')
 
 const root = execSync('npm root -g').toString().trim()
-const config = require(`${root}/@akijoey/prettier-config`)
+const name = '@akijoey/prettier-config'
+
+const config = require(`${root}/${name}`)
+
+Object.assign(config, {
+  plugins: config.plugins.map(plugin => {
+    return require.resolve(`${root}/${name}/node_modules/${plugin}`)
+  }),
+  overrides: [
+    {
+      files: '*.pug',
+      options: { parser: 'pug' }
+    },
+    {
+      files: '*.styl',
+      options: { parser: 'stylus' }
+    }
+  ]
+})
 
 module.exports = config
