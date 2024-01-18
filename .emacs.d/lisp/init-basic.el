96132925ff757b4694aa03f9dc2ee607db82f7b9
AkiJoey <akijoey@akijoey.com>
2024-01-18 15:43:59 +0800

    feat: split window and move

--- .emacs.d/lisp/init-basic.el
+++ .emacs.d/lisp/init-basic.el
@@ -7,6 +7,19 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+;; interactive function
+(defun windmove-split-right ()
+  "Split window horizontally and move right"
+  (interactive)
+  (split-window-right)
+  (windmove-right))
+
+(defun windmove-split-down ()
+  "Split window vertically and move down"
+  (interactive)
+  (split-window-below)
+  (windmove-down))
+
 ;; key bindings
 (global-set-key (kbd "C-x /") 'comment-line)
 
@@ -37,7 +50,9 @@
   ("C-c w <left>" . windmove-left)
   ("C-c w <right>" . windmove-right)
   ("C-c w <up>" . windmove-up)
-  ("C-c w <down>" . windmove-down))
+  ("C-c w <down>" . windmove-down)
+  ("C-c w |" . windmove-split-right)
+  ("C-c w -" . windmove-split-down))
 
 (use-package move-text
   :bind
